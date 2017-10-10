5c9cb8b2ad3eb3feaf023d8eaafeaf514f9076c1
zanjs <root@zanjs.com>
2017-10-10 15:55:24 +0800

    不想写代码 :snowman:

--- src/d_elements.js
+++ src/d_elements.js
@@ -2,23 +2,27 @@ var PLtmall = "#detail-base-smart-banner,#J_BottomSmartBanner,#J_BottomSmartBann
 
 var PLtaobao = "div[dpr],a[id$='_close'],a[id$='_open'],.bottom_bar,.item-action,.J_bottomBar,.install-app,.thw-smartbanner,.openapp,.download-adv,#J_JHSDownloadBar,#J_JHSDownloadMask,.thw-promotion-app,.thw-smartbanner,.thw-detail-panel";
 
+var PLJD = ".af-open-app-btn,div[id^='download-'],div[class^='download-'],.tryme,.cart-concern-btm-fixed,.topheader,.showfixedtop";
+
+var PLMLS = ".buy_box,.welcome_downLoad,.download_btn,.nine_box li:last-child";
+
 var PLElements = {
     "tmall.com": PLtmall,
     "taobao.com": PLtaobao,
     "yixun.com": ".goods_buy",    
-    "jd.com": ".tryme,.cart-concern-btm-fixed,.topheader,.showfixedtop",
-    "suning.com": ".detail-download,.shopping-car,.app-down,.sn-client-box,#s-actionBar-container,.detail-download,.detail-new-download",
+    "jd.com": PLJD,
+    "suning.com": "#clientHead,div[class$='-headDown'],.detail-download,.shopping-car,.app-down,.sn-client-box,#s-actionBar-container,.detail-download,.detail-new-download",
     "yhd.com": "#product-buy-num,.cart_fixed",
-    "dangdang.com": ".app-download-wrapper,.shopping_cart",
+    "dangdang.com": "#j_promotionPop,.ddapp-wrapper,.app_download,.app-download-wrapper,.shopping_cart",
     "vancl.com": "#floatBox,.buy",
     "amazon.com": "#smart-app-banner,#goldboxBuyBox",
     "mogujie.com": "#M_topLogin,.ui-hada,#DetailFootbar,#M_top,.ui-middle-banner,#downloadImg,.download_banner",
-    "meilishuo.com": "img,.buy_box,.welcome_downLoad,.download_btn,.nine_box li:last-child'",
+    "meilishuo.com": PLMLS,
     "smzdm.com": ".foot_banner,.download-logo,.download-normal,.download-mini,.fixed-bottom",
-    "vip.com": ".u-download-bar,.modal-body-noBotton,.app-download-bar,.fixed-bottom",
-    "jumei.com": "#jumei_location,.touch-footer-container,.bottom_fixed",
+    "vip.com": "#J-download-bar,#J-foot-download,.u-download-bar,.modal-body-noBotton,.app-download-bar,.fixed-bottom",
+    "jumei.com": ".download-btn,#jumei_location,.touch-footer-container,.bottom_fixed",
     "mbaobao.com": ".welcome-app-download,.buy-bar,.go-other",
-    "moonbasa.com": ".download_app,.good-menu-bar",
+    "moonbasa.com": "a[class^='download_'],.download_app,.good-menu-bar",
     "lefeng.com": ".ui-decoration-hyperlink",
     "paipai.com": ".mod_download,.cbox_hd",
     "com.cn": ".supernatant,.buy_area_now",
